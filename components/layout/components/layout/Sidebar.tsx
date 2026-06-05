b64d629d9b5ca1fa6554261b4a697697196da45c
ismahdeismail-beep <ismahdeismail@gmail.com>
2026-06-05 15:41:52 +0300

    Update Sidebar.tsx

--- components/layout/components/layout/Sidebar.tsx
+++ components/layout/components/layout/Sidebar.tsx
@@ -1,1 +1,24 @@
+import "./globals.css";
+import Sidebar from "../components/layout/Sidebar";
 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="flex min-h-screen bg-slate-50">
+
+        {/* SIDEBAR */}
+        <Sidebar />
+
+        {/* MAIN CONTENT */}
+        <main className="flex-1 p-4 md:p-6">
+          {children}
+        </main>
+
+      </body>
+    </html>
+  );
+}
